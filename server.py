4ff2494260c312c58c1715e463ee375dac9a364f
Devendra Kag <devendra.kag@calsoftinc.com>
2023-02-21 16:27:53 +0530

    Just for demo

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
+import http.client
+from functools import wraps
 import json
 from os import environ as env
 from urllib.parse import quote_plus, urlencode
+from urllib.request import urlopen
 
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, redirect, render_template, session, url_for
+from flask import Flask, jsonify, redirect, render_template, request, session, url_for, Response
+from typing import Dict
+from flask_cors import cross_origin
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
@@ -23,8 +28,7 @@ oauth.register(
         "scope": "openid profile email",
     },
     server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
-)
-
+)    
 
 @app.route("/login")
 def login():
@@ -57,6 +61,16 @@ def logout():
 @app.route("/")
 def home():
     return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+
+@app.route("/token")
+def get_token():
+    conn = http.client.HTTPSConnection("dev-0rllrgj10iy0ku46.us.auth0.com")
+    payload = "{\"client_id\":\"AH2ImNaSL01BILtIp53dJhCuQGyC3Sja\",\"client_secret\":\"PeagJFvhqsizo9zAnfTb1U-LNhB3pl2FAv2z_uByRGCq8PTdStiU2B7gO0qGivhR\",\"audience\":\"http://127.0.0.1:3000/api\",\"grant_type\":\"client_credentials\"}"
+    headers = { 'content-type': "application/json" }
+    conn.request("POST", "/oauth/token", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    return data
 
 
 if __name__ == "__main__":
